cf2aede9f07250250f19c5dc837094ac5c66d464
orcunenes <vvalentos.1@gmail.com>
2023-02-26 13:13:33 +0300

    First Progress

---
+++ PCSUGGESTER/mainpage.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PCSUGGESTER
+{
+    partial class mainpage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Selection = new System.Windows.Forms.Panel();
+            this.yourpc = new System.Windows.Forms.Label();
+            this.Graphics = new System.Windows.Forms.Label();
+            this.cpu = new System.Windows.Forms.Label();
+            this.ram = new System.Windows.Forms.Label();
+            this.storage = new System.Windows.Forms.Label();
+            this.Programs = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Selection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PC-SUGGESTER";
+            // 
+            // Selection
+            // 
+            this.Selection.Controls.Add(this.Programs);
+            this.Selection.Location = new System.Drawing.Point(216, 37);
+            this.Selection.Name = "Selection";
+            this.Selection.Size = new System.Drawing.Size(673, 457);
+            this.Selection.TabIndex = 1;
+            // 
+            // yourpc
+            // 
+            this.yourpc.AutoSize = true;
+            this.yourpc.Location = new System.Drawing.Point(14, 57);
+            this.yourpc.Name = "yourpc";
+            this.yourpc.Size = new System.Drawing.Size(55, 13);
+            this.yourpc.TabIndex = 2;
+            this.yourpc.Text = "YOUR PC";
+            // 
+            // Graphics
+            // 
+            this.Graphics.AutoSize = true;
+            this.Graphics.Location = new System.Drawing.Point(14, 84);
+            this.Graphics.Name = "Graphics";
+            this.Graphics.Size = new System.Drawing.Size(35, 13);
+            this.Graphics.TabIndex = 3;
+            this.Graphics.Text = "label2";
+            // 
+            // cpu
+            // 
+            this.cpu.AutoSize = true;
+            this.cpu.Location = new System.Drawing.Point(14, 112);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(35, 13);
+            this.cpu.TabIndex = 4;
+            this.cpu.Text = "label2";
+            // 
+            // ram
+            // 
+            this.ram.AutoSize = true;
+            this.ram.Location = new System.Drawing.Point(14, 138);
+            this.ram.Name = "ram";
+            this.ram.Size = new System.Drawing.Size(35, 13);
+            this.ram.TabIndex = 5;
+            this.ram.Text = "label3";
+            // 
+            // storage
+            // 
+            this.storage.AutoSize = true;
+            this.storage.Location = new System.Drawing.Point(14, 163);
+            this.storage.Name = "storage";
+            this.storage.Size = new System.Drawing.Size(35, 13);
+            this.storage.TabIndex = 6;
+            this.storage.Text = "label2";
+            // 
+            // Programs
+            // 
+            this.Programs.FormattingEnabled = true;
+            this.Programs.Location = new System.Drawing.Point(3, 3);
+            this.Programs.Name = "Programs";
+            this.Programs.Size = new System.Drawing.Size(202, 439);
+            this.Programs.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Programs";
+            // 
+            // mainpage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 506);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.storage);
+            this.Controls.Add(this.ram);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.Graphics);
+            this.Controls.Add(this.yourpc);
+            this.Controls.Add(this.Selection);
+            this.Controls.Add(this.label1);
+            this.Name = "mainpage";
+            this.Text = "PCS";
+            this.Load += new System.EventHandler(this.mainpage_Load);
+            this.Selection.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel Selection;
+        private System.Windows.Forms.Label yourpc;
+        private System.Windows.Forms.Label Graphics;
+        private System.Windows.Forms.Label cpu;
+        private System.Windows.Forms.Label ram;
+        private System.Windows.Forms.Label storage;
+        private System.Windows.Forms.CheckedListBox Programs;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PCSUGGESTER/mainpage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Management.Instrumentation;
+namespace PCSUGGESTER
+{
+    public partial class mainpage : Form
+    {
+        public mainpage()
+        {
+            InitializeComponent();
+        }
+
+        private void mainpage_Load(object sender, EventArgs e)
+        {
+            Graphics.Text = string.Empty;
+            cpu.Text=string.Empty;
+            ram.Text=string.Empty;
+            storage.Text=string.Empty;
+            ManagementObjectSearcher graphicsCardSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject obj in graphicsCardSearcher.Get())
+            {
+                Graphics.Text="Graphics Card :"+ obj["Name"];
+            }
+
+            // Get CPU information
+            ManagementObjectSearcher cpuSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject obj in cpuSearcher.Get())
+            {
+                cpu.Text="CPU :"+ obj["Name"];
+            }
+
+            // Get RAM information
+            ManagementObjectSearcher ramSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            long totalRam = 0;
+            foreach (ManagementObject obj in ramSearcher.Get())
+            {
+                totalRam += Convert.ToInt64(obj["Capacity"]);
+            }
+            ram.Text="RAM: GB"+ Math.Round(Convert.ToDouble(totalRam) / 1024 / 1024 / 1024, 2);
+
+            // Get storage information
+            ManagementObjectSearcher storageSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk WHERE DriveType=3");
+            foreach (ManagementObject obj in storageSearcher.Get())
+            {
+                storage.Text+="Drive: GB free"+ obj["Name"]+ Math.Round(Convert.ToDouble(obj["FreeSpace"]) / 1024 / 1024 / 1024, 2);
+            }
+
+            // Get OS information
+            ManagementObjectSearcher osSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            foreach (ManagementObject obj in osSearcher.Get())
+            {
+                Console.WriteLine("OS: {0} {1}", obj["Caption"], obj["OSArchitecture"]);
+            }
+        }
+    }
+}
